5685b050263290d2504560c90eeeecabad26442e
Alexandru Corut <r3dm4n@me.com>
2017-09-18 15:01:18 +0100

    added hamburger menu

--- chat-app/Controller/ChatVC.swift
+++ chat-app/Controller/ChatVC.swift
@@ -9,27 +9,15 @@
 import UIKit
 
 class ChatVC: UIViewController {
-
+    
+    @IBOutlet weak var menuBtn: UIButton!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
