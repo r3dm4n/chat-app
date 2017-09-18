dfd06871b6a425c99a9dae6045a90f6246324f3c
Alexandru Corut <r3dm4n@me.com>
2017-09-18 15:01:01 +0100

    added hamburger menu

---
+++ chat-app/Controller/ChannelVC.swift
@@ -0,0 +1,35 @@
+//
+//  ChannelVC.swift
+//  chat-app
+//
+//  Created by r3d on 18/09/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
