78397168eb45f2888882c7be855b1137e59235d5
Alexandru Corut <r3dm4n@me.com>
2017-09-19 17:49:11 +0100

    web req. for creating users

---
+++ chat-app/Services/AuthService.swift
@@ -0,0 +1,70 @@
+//
+//  AuthService.swift
+//  chat-app
+//
+//  Created by r3d on 19/09/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+
+    static let instance = AuthService()
+
+    let defaults = UserDefaults.standard
+
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        } set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        } set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+
+        let header = [
+            "Content-Type": "aplication/json; charset=utf-8"
+        ]
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responeString {
+            (response) in
+            if response.result.error == nil {
+                    completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+
+
+
+
+
+
+}
--- chat-app/Utils/Constants.swift
+++ chat-app/Utils/Constants.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+//Heroku
+let BASE_URL = "https://r3dm4n-chat-app.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
