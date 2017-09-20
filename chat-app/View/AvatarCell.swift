523b0c709beae22e5682d50c04aa752800aee81f
Alexandru Corut <r3dm4n@me.com>
2017-09-20 13:00:10 +0100

    added AvatarCell

---
+++ chat-app/View/AvatarCell.swift
@@ -0,0 +1,24 @@
+//
+//  AvatarCell.swift
+//  chat-app
+//
+//  Created by r3d on 20/09/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func setupView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
