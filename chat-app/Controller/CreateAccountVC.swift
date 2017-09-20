0980f7b7f3d18e1d265f23adf80ca185cd1027fc
Alexandru Corut <r3dm4n@me.com>
2017-09-20 14:59:15 +0100

    generate avatar background color + CircleImage View

--- chat-app/Controller/CreateAccountVC.swift
+++ chat-app/Controller/CreateAccountVC.swift
@@ -9,36 +9,40 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-    
+
     //Outlets
     @IBOutlet weak var usernameTxt: UITextField!
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
-    
+
     //Variables
     var avatarName = "profileDefault"
     var avatarColor = "[0.5, 0.5, 0.5, 1]"
-    
-    
+    var bgColor: UIColor?
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         if UserDataService.instance.avatarName != "" {
             userImg.image = UIImage(named: UserDataService.instance.avatarName)
             avatarName = UserDataService.instance.avatarName
+            if avatarName.contains("light") && bgColor == nil {
+                userImg.backgroundColor = UIColor.lightGray
+            }
         }
     }
-    
-    
+
+
     @IBAction func onCreateAccountPressed(_ sender: Any) {
         guard let name = usernameTxt.text, usernameTxt.text != "" else { return }
-        guard let email = emailTxt.text , emailTxt.text != "" else { return }
-        guard let password = passwordTxt.text , passwordTxt.text != "" else { return }
-        
+        guard let email = emailTxt.text, emailTxt.text != "" else { return }
+        guard let password = passwordTxt.text, passwordTxt.text != "" else { return }
+
         AuthService.instance.registerUser(email: email, password: password) { (success) in
             if success {
                 AuthService.instance.loginUser(email: email, password: password, completion: { (success) in
@@ -55,14 +59,20 @@ class CreateAccountVC: UIViewController {
             }
         }
     }
-    
+
     @IBAction func onPickAvatarPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_AVATAR_PICKER, sender: nil)
     }
-    
+
     @IBAction func onPickBgColorPressed(_ sender: Any) {
+        let r = CGFloat(arc4random_uniform(255)) / 255
+        let g = CGFloat(arc4random_uniform(255)) / 255
+        let b = CGFloat(arc4random_uniform(255)) / 255
+
+        bgColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        self.userImg.backgroundColor = bgColor
     }
-    
+
     @IBAction func onCloseBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
