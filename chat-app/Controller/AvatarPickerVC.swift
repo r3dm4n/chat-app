3557dd289e1fe1324ece5ea76f1a4a4e5bbf9830
Alexandru Corut <r3dm4n@me.com>
2017-09-20 13:40:20 +0100

    added func to change numOfColumn based on device size

--- chat-app/Controller/AvatarPickerVC.swift
+++ chat-app/Controller/AvatarPickerVC.swift
@@ -45,6 +45,20 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
         dismiss(animated: true, completion: nil)
     }
     
+    //change numOfColumn based on device size
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        var numOfColumns: CGFloat = 3
+        if UIScreen.main.bounds.width > 320 {
+            numOfColumns = 4
+        }
+        
+        let spaceBetweenCells: CGFloat = 10
+        let padding: CGFloat = 40
+        let cellDimension = ((collectionView.bounds.width - padding) - (numOfColumns - 1) * spaceBetweenCells) / numOfColumns
+        return CGSize(width: cellDimension, height: cellDimension)
+    }
+    
     @IBAction func onSegmentControlChanged(_ sender: Any) {
         if segmentControl.selectedSegmentIndex == 0 {
             avatarType = .dark
