d579d14149bd2f5b7ca9f8716f49ffe45fc85601
Alexandru Corut <r3dm4n@me.com>
2017-09-22 13:25:54 +0100

    created ChannelCell

---
+++ chat-app/View/ChannelCell.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelCell.swift
+//  chat-app
+//
+//  Created by r3d on 22/09/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+
+}
