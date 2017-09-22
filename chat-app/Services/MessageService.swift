36b5ed3975f6d2301f87cafcf84ea215c0aa42db
Alexandru Corut <r3dm4n@me.com>
2017-09-22 12:23:31 +0100

    added Channel + MessageService

---
+++ chat-app/Services/MessageService.swift
@@ -0,0 +1,19 @@
+//
+//  MessageService.swift
+//  chat-app
+//
+//  Created by r3d on 22/09/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import Foundation
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    
+    
+}
