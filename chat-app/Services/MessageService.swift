f710c84d5f3f55fffca0645aadda0469cc3013b6
Alexandru Corut <r3dm4n@me.com>
2017-09-22 12:33:17 +0100

    parsing channels

--- chat-app/Services/MessageService.swift
+++ chat-app/Services/MessageService.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
 
 class MessageService {
     
@@ -14,6 +16,28 @@ class MessageService {
     
     var channels = [Channel]()
     
-    
+    func findAllChannels(completion: @escaping CompletionHandler) {
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+                if let json =  JSON(data: data).array {
+                    for item in json {
+                        let name = item["name"].stringValue
+                        let channelDescription = item["description"].stringValue
+                        let id = item["_id"].stringValue
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                        
+                        self.channels.append(channel)
+                        completion(true)
+                    }
+                }
+                
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
     
 }
