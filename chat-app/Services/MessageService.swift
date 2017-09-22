e0c891633683c1f152f47ba2757c938534542ce6
Alexandru Corut <r3dm4n@me.com>
2017-09-22 12:47:00 +0100

    reindent + bug fix MessageService

--- chat-app/Services/MessageService.swift
+++ chat-app/Services/MessageService.swift
@@ -11,33 +11,33 @@ import Alamofire
 import SwiftyJSON
 
 class MessageService {
-    
+
     static let instance = MessageService()
-    
+
     var channels = [Channel]()
-    
+
     func findAllChannels(completion: @escaping CompletionHandler) {
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
-            
+
             if response.result.error == nil {
                 guard let data = response.data else { return }
-                if let json =  JSON(data: data).array {
+                if let json = JSON(data: data).array {
                     for item in json {
                         let name = item["name"].stringValue
                         let channelDescription = item["description"].stringValue
                         let id = item["_id"].stringValue
                         let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
-                        
                         self.channels.append(channel)
-                        completion(true)
                     }
+                    print(self.channels[0].channelTitle)
+                    completion(true)
                 }
-                
+
             } else {
                 completion(false)
                 debugPrint(response.result.error as Any)
             }
         }
     }
-    
+
 }
